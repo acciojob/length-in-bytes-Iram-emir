3d1861a6a42d6e69e8280e9767e1b07c2e30989b
iram-emir <158573737+Iram-emir@users.noreply.github.com>
2024-05-07 19:27:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-const byteSize = (str) => {
-  // write your code here
-	
-};function byteSize(str) {
-  // Create a Blob object from the input string
+
+function byteSize(str) {
   const blob = new Blob([str]);
-
-  // Get the size of the Blob object in bytes
   const sizeInBytes = blob.size;
-
   return sizeInBytes;
 }
 
-// Test cases
-console.log(byteSize('hello world')); // Output: 11
-console.log(byteSize('안녕하세요'));    // Output: 15
-console.log(byteSize(''));             // Output: 0
+console.log(byteSize('hello world'));  // Output: 11
+console.log(byteSize('안녕하세요'));     // Output: 15
+console.log(byteSize(''));              // Output: 0
 
-
+// Do not change the code below
+const str = prompt("Enter some string.");
+alert(byteSize(str));
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
